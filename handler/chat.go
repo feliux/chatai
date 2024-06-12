c50d1fa5be95b2d1114a3ae72657062196330e06
feliux <ahffro@gmail.com>
2024-06-13 00:29:46 +0200

    [refactor]

--- handler/chat.go
+++ handler/chat.go
@@ -2,37 +2,13 @@ package handler
 
 import (
 	"bytes"
-	"context"
 	"encoding/json"
-	"fmt"
-	"log"
+	"log/slog"
 	"net/http"
 
 	"github.com/feliux/chatai/view/chat"
 	"github.com/gorilla/websocket"
 )
-
-func HandleChatIndex(w http.ResponseWriter, r *http.Request) error {
-	return render(w, r, chat.Index())
-}
-
-var upgrader = websocket.Upgrader{
-	ReadBufferSize:  1024,
-	WriteBufferSize: 1024,
-}
-
-func HandleChat(w http.ResponseWriter, r *http.Request) error {
-	ws, err := upgrader.Upgrade(w, r, nil)
-	if err != nil {
-		log.Println("error handle chat", err)
-		return err
-	}
-	defer ws.Close()
-	for {
-		ChatLoop(ws, r.Context())
-	}
-	return nil
-}
 
 type HTMXMessage struct {
 	ChatMessage string `json:"chat_message"`
@@ -45,17 +21,42 @@ type HTMXMessage struct {
 	} `json:"HEADERS"`
 }
 
-func ChatLoop(ws *websocket.Conn, ctx context.Context) {
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+func HandleChatIndex(w http.ResponseWriter, r *http.Request) error {
+	return render(w, r, chat.Index())
+}
+
+func HandleChat(w http.ResponseWriter, r *http.Request) error {
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		slog.Error("error upgrading to websocket", "err", err)
+	}
+	defer ws.Close()
+	for {
+		Chat(w, r, ws)
+	}
+	return nil
+}
+
+func Chat(w http.ResponseWriter, r *http.Request, ws *websocket.Conn) {
 	_, p, err := ws.ReadMessage()
 	if err != nil {
-		fmt.Println(err)
+		slog.Error("error reading message from websocket", "err", err)
 	}
 	var msg HTMXMessage
 	json.Unmarshal(p, &msg)
 	var buf bytes.Buffer
-	chat.SentAndRecv(msg.ChatMessage, "You're an idiot").Render(ctx, &buf)
+	// err = render(w, r, chat.SentAndRecv(msg.ChatMessage, "This is a message from the bot!"))
+	// if err != nil {
+	// 	slog.Error("error rendering SentAndRecv template", "err", err)
+	// }
+	chat.SentAndRecv(msg.ChatMessage, "This is a message from the bot!").Render(r.Context(), &buf)
 	err = ws.WriteMessage(websocket.TextMessage, buf.Bytes())
 	if err != nil {
-		log.Println("error in ws ", err)
+		slog.Error("error writing message to websocket", "err", err)
 	}
 }
